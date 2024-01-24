06a5e7576790a5bc52bef0136183d0d585bd5c66
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2024-01-24 13:30:19 +0600

    try to fix few things

--- src/components/TaskModalContent .js
+++ src/components/TaskModalContent .js
@@ -10,14 +10,40 @@ const TaskModalContent = ({ onAddTask, onClose }) => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={taskTitle}
-        onChange={(e) => setTaskTitle(e.target.value)}
-        placeholder="Enter task title"
-      />
-      <button onClick={handleAddTask}>Add Task</button>
+    <div className="modal-box bg-[#EEEFF0] ">
+      <form method="dialog">
+        <button
+          onClick={onClose}
+          className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+        >
+          X
+        </button>
+      </form>
+
+      <div>
+        <label
+          htmlFor="taskName"
+          className="mb-2 inline-block text-sm text-dark sm:text-base"
+        >
+          Task Name
+        </label>
+        <input
+          type="text"
+          value={taskTitle}
+          onChange={(e) => setTaskTitle(e.target.value)}
+          placeholder="Enter task title"
+          className="w-full text-sm rounded border bg-gray-50 px-3 py-2 text-dark outline-none"
+        />
+      </div>
+      <div className="col-span-2 mt-6">
+        <button
+          type="submit"
+          className="btn w-full btn-sm btn-neutral"
+          onClick={handleAddTask}
+        >
+          Add Task
+        </button>
+      </div>
     </div>
   );
 };
