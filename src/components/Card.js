346f3e2bafa661184f07e424f14276ca808ab86b
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2024-01-24 11:05:43 +0600

    try to fix task drag and drop issue

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,36 @@
 import React from "react";
-import { Draggable } from "react-beautiful-dnd";
+import { Draggable, Droppable } from "react-beautiful-dnd";
 
 const Card = ({ card, index }) => {
   const { id, title, description, attachments } = card;
   return (
-    <Draggable className="mb-4 p-2 bg-gray-700 rounded shadow">
-      <h3 className="text-lg text-white font-semibold mb-2">{title}</h3>
-      {description && <p className="text-gray-600 mb-2">{description}</p>}
-      {attachments && (
-        <div>
-          <strong>Attachments:</strong> {attachments}
+    <Droppable droppableId={String(id)}>
+      {(provided) => (
+        <div ref={provided.innerRef} {...provided.droppableProps}>
+          <Draggable draggableId={String(id)} index={index}>
+            {(provided) => (
+              <div
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                ref={provided.innerRef}
+                className="mb-4 p-2 bg-white rounded shadow"
+              >
+                <h3 className="text-lg font-semibold mb-2">{title}</h3>
+                {description && (
+                  <p className="text-gray-600 mb-2">{description}</p>
+                )}
+                {attachments && (
+                  <div>
+                    <strong>Attachments:</strong> {attachments}
+                  </div>
+                )}
+              </div>
+            )}
+          </Draggable>
+          {provided.placeholder}
         </div>
       )}
-    </Draggable>
+    </Droppable>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { DragDropContext } from "react-beautiful-dnd";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <DragDropContext>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </DragDropContext>
 );
 
 // If you want to start measuring performance in your app, pass a function
